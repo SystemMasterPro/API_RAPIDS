f9b2e104ea660f01b264c856ec22a0ba536a063c
Richard <richardjimenez.9641@gmail.com>
2021-07-16 17:14:27 -0500

    BD en conflicto

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -158,3 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
