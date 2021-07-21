0d546e48f97af786f64e4fe2d7c84b5fb48ae2b0
Richard <richardjimenez.9641@gmail.com>
2021-07-20 23:25:01 -0500

    ImageProblem

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(&qc99c1jy&i@m0$3g#g6fzz=&2^1#5qa3pjn%dc!!-=qixc6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
